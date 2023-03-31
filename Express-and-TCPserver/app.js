bad88e519a47fec3af58d41ba30da752f532deb5
ZhongKuo0228 <rt019623@gmail.com>
2023-03-31 15:50:43 +0800

    [ADD]create basic express server setting

---
+++ Express-and-TCPserver/app.js
@@ -0,0 +1,21 @@
+//---Modules Setting----------------------------------
+import express from "express";
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//---router----------------------------------------
+
+//---listen-----------------------------------------
+import dotenv from "dotenv";
+dotenv.config();
+
+app.listen(process.env.EXPRESS_SERVER_PORT, () => {
+    console.log("Express Server Is Running!");
+});
+//---view-------------------------------------------
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", function (req, res, next) {
+    res.render("index.ejs");
+});
