c362bb875865a06c408dd59069386dbf724521ce
ZhongKuo0228 <rt019623@gmail.com>
2023-03-31 20:33:59 +0800

    [ADD] Send data from frontend PLpage to Express and receive response in console.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import WriteCode from "./components/WriteCode";
-import Console from "./components/Console";
+import WriteCode from "./pages/PLcode/PLcode";
+
 import styled from "styled-components";
 
 const Wrap = styled.div`
@@ -13,7 +13,7 @@ const App = () => {
             <header>我是標頭</header>
             <Wrap>
                 <WriteCode />
-                <Console />
+
             </Wrap>
         </>
     );
