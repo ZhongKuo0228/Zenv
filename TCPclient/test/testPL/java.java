73fbfdc5bbfd507c9e986afe33f57d03eadef225
ZhongKuo0228 <rt019623@gmail.com>
2023-04-03 12:28:53 +0800

    [ADD] Completed testing program language By using Docker image

---
+++ TCPclient/test/testPL/java.java
@@ -0,0 +1,9 @@
+public class TestImage {
+    public static void main(String[] args) {
+        System.out.println("test openJDK image");
+    }
+}
+
+// docker run -it -v
+// /Users/zhongkuo/Desktop/Back-End-Class-Batch19/Zenv/TCPclient/test/testPL/java.java:/app/java.java
+// openjdk:21-ea-15-slim-buster java /app/java.java
